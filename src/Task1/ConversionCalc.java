409721488883929605eae439c814936faa7bd329
root <robmadeyou@gmail.com>
2013-09-22 04:12:11 +0100

    Added comments and cleaned some of the code. Have fun

--- src/Task1/ConversionCalc.java
+++ src/Task1/ConversionCalc.java
@@ -1,13 +1,11 @@
 package Task1;
 
-import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
@@ -37,6 +35,9 @@ public class ConversionCalc extends JFrame{
 	public ConversionCalc(){
 		super("Conversion Calc");
 		
+		/*
+		 * Creating various elements from buttons, to Text fields!
+		 */
 		button1 = new JButton();
 		button1.setText("Convert!");
 		button1.setBounds(90, 120, 100, 20);
@@ -80,12 +81,24 @@ public class ConversionCalc extends JFrame{
 		
 		panel = new JPanel();
 		panel.setLayout(null);
+		/*
+		 * End of creating elements
+		 */
 		
+		
+		/*
+		 * Creating our handler (class down below)
+		 */
 		Handler handler = new Handler();
-		
+		//adding item to handler so once the button is pressed a certain something is done
 		button1.addActionListener(handler);
-		panel.add(button1);
 		
+		/*
+		 * Adding everything to the panel and then
+		 * adding the panel to the frame, this way we can
+		 * actually see the items being rendered!
+		 */
+		panel.add(button1);
 		
 		panel.add(miles);
 		panel.add(inches);
@@ -102,8 +115,14 @@ public class ConversionCalc extends JFrame{
 		
 		
 	}
+	
 	private static ConversionCalc calc;
+	
 	public static void main(String args[]){
+		/*
+		 * Setting the system default look and feel, meaning that what ever the
+		 * system default theme is - java will use it and adapt their gui to that
+		 */
 		try{
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		}catch(Exception e){
@@ -111,12 +130,22 @@ public class ConversionCalc extends JFrame{
 		}
 		
 		calc = new ConversionCalc();
+		/*
+		 * Dispose on close means that it will get rid of the frame - with this you can have multiple frames and once X is clicked
+		 * instead of killing the whole script it would only kill the frame created. Good if you have multiple windows being
+		 * constantly opened
+		 */
 		calc.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		
+		calc.setLocation(400, 400);
 		calc.setSize(280, 200);
 		calc.setVisible(true);
 	}
 	
+	
+	/*
+	 * Action listener, will listen if any even is called, for example a button pressed or
+	 * text is entered
+	 */
 	public class Handler implements ActionListener{
 		public void actionPerformed(ActionEvent event){
 			float mileToKm = 1.60935f;
@@ -124,25 +153,27 @@ public class ConversionCalc extends JFrame{
 			float kgToPound = 2.2046f;
 			
 			
-			panel.add(miles);
-			panel.add(inches);
-			panel.add(kilos);
-			
-			panel.add(kilometers);
-			panel.add(cm);
-			panel.add(pounds);
+			/*
+			 * Actual conversion happening here
+			 * 
+			 * this if statement checks if button one was the source
+			 * of the event, if it was then everything inside the statement is run
+			 * and so we see the values change to the new ones in the program
+			 */
 			
 			if(event.getSource() == button1){
+				/*
+				 * I rounded the values because they returned really long doubles
+				 * which made it a pain to read :s
+				 */
 				double km = Math.round(Double.parseDouble(miles.getText()) * mileToKm);
 				kilometers.setText(km + "");
 				double centm = Math.round(Double.parseDouble(inches.getText()) * inchToCm);
 				cm.setText(centm + "");
 				double pound = Math.round(Double.parseDouble(kilos.getText()) * kgToPound);
 				pounds.setText(pound + "");
-				
-				
-				
 			}
+			//possibly add another button to convert the values back?
 		}
 	}
 	
