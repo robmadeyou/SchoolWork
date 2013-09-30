809c6cf06b70b24e8c338f34d7d4f7c55987d914
Robert Ellis <robmadeyou@gmail.com>
2013-09-30 13:07:14 +0100

    Maths

--- src/Task2/ProblemSolver.java
+++ src/Task2/ProblemSolver.java
@@ -6,10 +6,10 @@ public class ProblemSolver {
 		int a = 6,b = 83,c = 12,d = 95,e = 5;
 		System.out.println("The difference between b and c is: " + (b - c));
 		System.out.println("The sum of A, C and E is: " + (a+c+e));
-		System.out.println("The product of A and E is; ");
+		System.out.println("The product of A and E is; " + a * e);
 		System.out.println("D divided by E: " + d / e);
 		System.out.println("Total of all 5 numbers: " + (a+b+c+d+e));
-		System.out.println("Ttoal of all 5 numbers + 100: " + (a+b+c+d+e+100));
+		System.out.println("Total of all 5 numbers + 100: " + (a+b+c+d+e+100));
 	}
 
 }
